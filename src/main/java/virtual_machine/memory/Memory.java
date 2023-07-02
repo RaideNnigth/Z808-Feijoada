1ec826b64fc3c1006d0f04f96ff28cc05f1cb519
Rávilon <ravilon.ads@inf.ufpel.edu.br>
2023-07-02 16:08:40 -0300

    Create Memory.java
    Memory Class created in package:  virtual_machine.memory

---
+++ src/main/java/virtual_machine/memory/Memory.java
@@ -0,0 +1,26 @@
+package virtual_machine.memory;
+import virtual_machine.types.UnsignedShort;
+
+public class Memory implements InstructionMemory, DataMemory {
+    private final UnsignedShort[] memory = new UnsignedShort[65_536];
+    public Memory() {
+        for (int i = 0; i < 65_536; i++) {
+            this.memory[i] = new UnsignedShort(0);
+        }
+    }
+    @Override
+    public UnsignedShort[] getInstance() {
+        return this.memory;
+    }
+
+    @Override
+    public void write(UnsignedShort value, UnsignedShort address) {
+        this.memory[address.getValue()] = value;
+    }
+
+    @Override
+    public UnsignedShort read(UnsignedShort address) {
+        this.memory[address.getValue()] = address;
+        return address;
+    }
+}
