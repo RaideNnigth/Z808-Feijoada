55e7097bb56e801db09031add655cce772ea4f93
Henrique Rodrigues <96885946+Equiel-1703@users.noreply.github.com>
2023-07-02 17:55:33 -0300

    Fuckin' signed byte fix

--- src/main/java/virtual_machine/memory/InstructionMemory.java
+++
@@ -1,9 +0,0 @@
-package virtual_machine.memory;
-
-import virtual_machine.types.UnsignedShort;
-
-public interface InstructionMemory {
-    public Memory getInstance();
-    public void write(UnsignedShort value, UnsignedShort address);
-    public UnsignedShort read(UnsignedShort address);
-}
--- src/main/java/virtual_machine/memory/Memory.java
+++ src/main/java/virtual_machine/memory/Memory.java
@@ -1,24 +1,29 @@
 package virtual_machine.memory;
+
 import virtual_machine.types.UnsignedShort;
 
-public class Memory implements InstructionMemory, DataMemory {
-    private final UnsignedShort[] mainMemory = new UnsignedShort[65_536];
-    public Memory() {
+public class Memory {
+    private final UnsignedShort[] mainMemory;
+    private static Memory memInstance = null;
+
+    private Memory() {
+        mainMemory = new UnsignedShort[65_536];
         for (int i = 0; i < 65_536; i++) {
             this.mainMemory[i] = new UnsignedShort(0);
         }
     }
-    @Override
-    public Memory getInstance() {
-        return this;
+
+    public static Memory getInstance() {
+        if (memInstance == null)
+            memInstance = new Memory();
+
+        return memInstance;
     }
 
-    @Override
     public void write(UnsignedShort value, UnsignedShort address) {
         this.mainMemory[address.getValue()] = value;
     }
 
-    @Override
     public UnsignedShort read(UnsignedShort address) {
         return this.mainMemory[address.getValue()];
     }
