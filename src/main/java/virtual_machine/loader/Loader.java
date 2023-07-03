2b3b23a235cc8181c8bc4eebae1a49bb9fb34d68
Henrique Rodrigues <96885946+Equiel-1703@users.noreply.github.com>
2023-07-02 21:17:46 -0300

    Loader Update

--- src/main/java/virtual_machine/loader/Loader.java
+++ src/main/java/virtual_machine/loader/Loader.java
@@ -3,12 +3,43 @@ package virtual_machine.loader;
 import virtual_machine.memory.MemoryController;
 
 import java.io.FileInputStream;
-import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Loader {
     private String filePath;
-    private FileInputStream programFile;
+    private FileInputStream programFileReader;
     private MemoryController memoryController;
 
+    public Loader(String path) {
+        memoryController = new MemoryController();
+        setPathToProgram(path);
+    }
 
+    public void setPathToProgram(String path) {
+        filePath = path;
+
+        try {
+            programFileReader = new FileInputStream(filePath);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Arquivo não encontrado!");
+        }
+    }
+
+    public boolean loadToMemory() {
+        // Reading header
+        try {
+            int CS_Start = programFileReader.read();
+            int CS_End = programFileReader.read();
+            int DS_Start = programFileReader.read();
+            int DS_End = programFileReader.read();
+            int SS_Start = programFileReader.read();
+            int SS_End = programFileReader.read();
+        } catch (IOException e) {
+            System.out.println("Deu bosta");
+        }
+
+        return true;
+    }
 }
---
+++ src/main/java/virtual_machine/loader/TempMain.java
@@ -0,0 +1,7 @@
+package virtual_machine.loader;
+
+public class TempMain {
+    public static void main(String[] args) {
+        
+    }
+}
