eb84d3e97bde5b8ad16ddec0e84e4bcf1e5913e0
Henrique Rodrigues <96885946+Equiel-1703@users.noreply.github.com>
2023-07-02 21:06:00 -0300

    Implementing Loader

--- src/main/java/virtual_machine/loader/Loader.java
+++ src/main/java/virtual_machine/loader/Loader.java
@@ -1,5 +1,14 @@
 package virtual_machine.loader;
 
+import virtual_machine.memory.MemoryController;
+
+import java.io.FileInputStream;
+import java.util.Scanner;
+
 public class Loader {
+    private String filePath;
+    private FileInputStream programFile;
+    private MemoryController memoryController;
+
 
 }
