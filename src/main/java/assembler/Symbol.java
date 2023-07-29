8b03d0478c30527bc30f43a8c55030147beb76d3
Gabriel Leite Bessa <gabrielleitebessa@gmail.com>
2023-07-29 16:21:51 -0300

    Adicionando a classe Symbol

---
+++ src/main/java/assembler/Symbol.java
@@ -0,0 +1,11 @@
+package assembler;
+
+import java.util.ArrayList;
+
+public class Symbol {
+    private String identificator;
+    private boolean isDeclared;
+    private ArrayList<Short> usedAt;
+
+
+}
