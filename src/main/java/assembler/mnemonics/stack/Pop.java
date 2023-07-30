3ff6c7a27ec6693fb1091d3e17f76a06f4187793
Rávilon <ravilon.ads@inf.ufpel.edu.br>
2023-07-30 18:25:40 -0300

    Update Pop.java
    Class created on operations

--- src/main/java/assembler/mnemonics/stack/Pop.java
+++ src/main/java/assembler/mnemonics/stack/Pop.java
@@ -1,4 +1,24 @@
 package assembler.mnemonics.stack;
 
-public class Pop {
+import assembler.Assembler;
+import assembler.mnemonics.Operation;
+import assembler.utils.AssemblerUtils;
+
+import java.util.LinkedList;
+
+public class Pop extends Operation {
+    public static final String MNEMONIC = "POP";
+    private final short POP_AX = 0x58FF;
+
+    @Override
+    public void assemble(String line) {
+        String[] tokens = AssemblerUtils.decomposeInTokens(line);
+
+        LinkedList<Short> assembledCode = Assembler.getInstance().getAssembledCode();
+
+        if (tokens[1].equals(AX_STR)) {
+            // AX POP
+            assembledCode.add(POP_AX);
+        }
+    }
 }
