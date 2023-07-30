222c6436f4345703b8db3b515aa653bd2cde9098
Henrique Rodrigues <96885946+Equiel-1703@users.noreply.github.com>
2023-07-30 00:17:14 -0300

    Implemented ADD in Assembler

--- src/main/java/assembler/codeprocessors/LabelProcessor.java
+++ src/main/java/assembler/codeprocessors/LabelProcessor.java
@@ -16,6 +16,6 @@ public class LabelProcessor {
             return;
 
         Symbol symbol = new Symbol(label, true, (short) Assembler.getInstance().getPC());
-        Assembler.getInstance().getSymbolTable().put(label, symbol);
+        //Assembler.getInstance().getSymbolTable().put(label, symbol);
     }
 }
