63e77f1a240543d47819dcd0b59f06e8c255b849
Gustavo Pinzon Pereira <32282947+RaideNnigth@users.noreply.github.com>
2023-07-30 18:11:50 -0300

    AND,NOT,XOR
    Adding to assembler and not and xor

--- src/main/java/assembler/mnemonics/logical/Xor.java
+++ src/main/java/assembler/mnemonics/logical/Xor.java
@@ -6,11 +6,11 @@ import assembler.utils.AssemblerUtils;
 
 import java.util.LinkedList;
 
-public class And extends Operation {
-    public static final String MNEMONIC = "AND";
-    private final short AND_AXAX = 0x23C0;
-    private final short AND_AXDX = 0x23C2;
-    private final short AND_AXCTE = 0x25FF;
+public class Xor extends Operation {
+    public static final String MNEMONIC = "XOR";
+    private final short XOR_AXAX = 0x33C0;
+    private final short XOR_AXDX = 0x33C2;
+    private final short XOR_AXCTE = 0x35FF;
     @Override
     public void assemble( String line ) {
         String[] tokens = AssemblerUtils.decomposeInTokens(line);
@@ -18,17 +18,17 @@ public class And extends Operation {
         LinkedList<Short> assembledCode = Assembler.getInstance().getAssembledCode();
 
         if (tokens[1].equals(AX_STR)) {
-            // AX && AX
+            // AX xor AX
             if (tokens[2].equals(AX_STR)) {
-                assembledCode.add(AND_AXAX);
+                assembledCode.add(XOR_AXAX);
             }
-            // AX && DX
+            // AX xor DX
             else if (tokens[2].equals(DX_STR)) {
-                assembledCode.add(AND_AXDX);
+                assembledCode.add(XOR_AXDX);
             }
             // Check for immediate numeric value
             else if (AssemblerUtils.isNumericConstant(tokens[2])) {
-                assembledCode.add(AND_AXCTE);
+                assembledCode.add(XOR_AXCTE);
                 assembledCode.add(AssemblerUtils.convertNumber(tokens[2]));
             }
         }
